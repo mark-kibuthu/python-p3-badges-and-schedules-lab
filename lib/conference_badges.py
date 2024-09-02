4d11108bfc2ae051dec2b02618792eebaafea64f
Agent-grey <markkibuthu@gmail.com>
2024-09-02 11:45:03 +0300

    complete lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,14 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    return [badge_maker(name) for name in names]
 
-def assign_rooms(names):
-    return None
+def assign_rooms(speakers):
+    return [f"Hello, {speaker}! You'll be assigned to room {index + 1}!" for index, speaker in enumerate(speakers)]
 
-def printer(names):
-    return None
+def printer(speakers):
+    for badge in batch_badge_creator(speakers):
+        print(badge)
+    for room_assignment in assign_rooms(speakers):
+        print(room_assignment)
